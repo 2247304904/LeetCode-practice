721e3aeb67d33255e72514f1a36d5307f606655f
22473 <2247304904@qq.com>
2021-11-15 18:00:13 +0800

    剑指offer 14-I 剪绳子

---
+++ Day01/src/main/java/com/zpy/day01/Test02.java
@@ -0,0 +1,21 @@
+package com.zpy.day01;
+
+/**
+ * 剑指offer 14-I 剪绳子
+ */
+public class Test02 {
+
+    public int cuttingRope(int n) {//暴力解题
+        int mid = n / 2;
+        for (int i = 0; i < mid; i++) {
+            if()
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Test02 test02 = new Test02();
+        int i = test02.cuttingRope(5);
+        System.out.println(i);
+    }
+}
