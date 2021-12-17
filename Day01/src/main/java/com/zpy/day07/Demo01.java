b9cdd768c9e30876768c285df5f8df830c7674c2
张鹏宇 <2247304904@qq.com>
2021-12-17 13:04:54 +0800

    剑指 Offer 25. 合并两个排序的链表

---
+++ Day01/src/main/java/com/zpy/day07/Demo01.java
@@ -0,0 +1,34 @@
+package com.zpy.day07;
+
+/**
+ * 剑指 Offer 25. 合并两个排序的链表
+ * https://leetcode-cn.com/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/
+ *
+ * @author 张鹏宇
+ * @date 2021/12/17 12:48
+ **/
+
+public class Demo01 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        int length1 = 0;
+        int length2 = 0;
+        ListNode node = null;
+        if (l1 != null) {
+            node = l1;
+            while (node != null) {
+                length1++;
+                node = node.next;
+            }
+        }
+        if (l2 != null) {
+            node = l2;
+            while (node != null) {
+                length2++;
+                node = node.next;
+            }
+        }
+        int min = Math.min(length1, length2);
+
+        return null;
+    }
+}
---
+++ Day01/src/main/java/com/zpy/day07/ListNode.java
@@ -0,0 +1,15 @@
+package com.zpy.day07;
+
+/**
+ * @author 张鹏宇
+ * @date 2021/12/17 12:49
+ **/
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
