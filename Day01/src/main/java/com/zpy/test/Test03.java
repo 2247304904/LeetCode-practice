b39b2b10e7cbcd59130f1f906b69748a3d1043b5
张鹏宇 <2247304904@qq.com>
2021-12-20 10:50:01 +0800

    测试 构造方法是否可以嵌套 结论：可以嵌套 使用this关键字 this在构造方法中 只能放在第一行

---
+++ Day01/src/main/java/com/zpy/test/Test03.java
@@ -0,0 +1,22 @@
+package com.zpy.test;
+
+/**
+ * @author 张鹏宇
+ * @date 2021/12/20 09:53
+ **/
+
+public class Test03 {
+    private String name;
+
+    public Test03() {
+
+    }
+
+    public Test03(String name) {
+        this();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
