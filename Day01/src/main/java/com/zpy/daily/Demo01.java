5e7fe387229b217404cb9e4366971b2282430f09
张鹏宇 <2247304904@qq.com>
2021-12-21 10:18:13 +0800

    LeetCode 每日一题 1154. 一年中的第几天

--- Day01/src/main/java/com/zpy/daily/Demo01.java
+++ Day01/src/main/java/com/zpy/daily/Demo01.java
@@ -17,7 +17,7 @@ public class Demo01 {
         int[] run = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int[] ping = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-        if (year % 4 == 0 || year % 400 == 0) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             for(int i = 0 ;i<mouth-1;i++){
                 day +=run[i];
             }
