f72406ecb76ad961d4d3541d7379ab024facac4d
张鹏宇 <2247304904@qq.com>
2021-12-22 11:23:06 +0800

    LeetCode : sql_test 595. 大的国家

--- Day01/src/main/java/com/zpy/sql_test/leetcode/demo01.sql
+++ Day01/src/main/java/com/zpy/sql_test/leetcode/demo01.sql
@@ -1,7 +1,7 @@
 -- 595. 大的国家
 -- https://leetcode-cn.com/problems/big-countries/
 
-select name,population,area  from world where area > 3000000 or  population > 25000000
+select name,population,area  from world where area >= 3000000 or  population >= 25000000
 
 -- or 会引起索引失效
 -- 通常使用union代替or
