7db3fde55b96e3cf988cab9d8e42ec1f384cd9f0
张鹏宇 <2247304904@qq.com>
2021-12-23 15:46:57 +0800

    NowCoder: sql_test 复习简单的语句 # SQL21 浙江大学用户题目回答情况

--- Day01/src/main/java/com/zpy/sql_test/nowcoder/demo01.sql
+++ Day01/src/main/java/com/zpy/sql_test/nowcoder/demo01.sql
@@ -111,3 +111,63 @@ from
     user_profile
 GROUP BY university
 ORDER BY avg_question_cnt
+
+
+
+drop table if exists `user_profile`;
+drop table if  exists `question_practice_detail`;
+CREATE TABLE `user_profile` (
+                                `id` int NOT NULL,
+                                `device_id` int NOT NULL,
+                                `gender` varchar(14) NOT NULL,
+                                `age` int ,
+                                `university` varchar(32) NOT NULL,
+                                `gpa` float,
+                                `active_days_within_30` int ,
+                                `question_cnt` int ,
+                                `answer_cnt` int
+);
+CREATE TABLE `question_practice_detail` (
+                                            `id` int NOT NULL,
+                                            `device_id` int NOT NULL,
+                                            `question_id`int NOT NULL,
+                                            `result` varchar(32) NOT NULL
+);
+INSERT INTO user_profile VALUES(1,2138,'male',21,'北京大学',3.4,7,2,12);
+INSERT INTO user_profile VALUES(2,3214,'male',null,'复旦大学',4.0,15,5,25);
+INSERT INTO user_profile VALUES(3,6543,'female',20,'北京大学',3.2,12,3,30);
+INSERT INTO user_profile VALUES(4,2315,'female',23,'浙江大学',3.6,5,1,2);
+INSERT INTO user_profile VALUES(5,5432,'male',25,'山东大学',3.8,20,15,70);
+INSERT INTO user_profile VALUES(6,2131,'male',28,'山东大学',3.3,15,7,13);
+INSERT INTO user_profile VALUES(7,4321,'male',28,'复旦大学',3.6,9,6,52);
+INSERT INTO question_practice_detail VALUES(1,2138,111,'wrong');
+INSERT INTO question_practice_detail VALUES(2,3214,112,'wrong');
+INSERT INTO question_practice_detail VALUES(3,3214,113,'wrong');
+INSERT INTO question_practice_detail VALUES(4,6543,111,'right');
+INSERT INTO question_practice_detail VALUES(5,2315,115,'right');
+INSERT INTO question_practice_detail VALUES(6,2315,116,'right');
+INSERT INTO question_practice_detail VALUES(7,2315,117,'wrong');
+INSERT INTO question_practice_detail VALUES(8,5432,118,'wrong');
+INSERT INTO question_practice_detail VALUES(9,5432,112,'wrong');
+INSERT INTO question_practice_detail VALUES(10,2131,114,'right');
+INSERT INTO question_practice_detail VALUES(11,5432,113,'wrong');
+
+SELECT * FROM user_profile
+
+# SQL21 浙江大学用户题目回答情况
+
+SELECT
+    u.device_id,
+    d.question_id,
+    d.result
+
+from user_profile u,
+     question_practice_detail d
+where
+        u.device_id = d.device_id
+  and u.university='浙江大学'
+
+
+
+
+
