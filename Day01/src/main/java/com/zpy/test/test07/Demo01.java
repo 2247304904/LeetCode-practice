b77e3fcbf4275041e1417403aeb8d1a806f1d56d
张鹏宇 <2247304904@qq.com>
2022-04-01 18:51:48 +0800

    2022-4-1 算法学习 反转链表

--- Day01/src/main/java/com/zpy/test/test07/Demo01.java
+++ Day01/src/main/java/com/zpy/test/test07/Demo01.java
@@ -8,12 +8,49 @@ package com.zpy.test.test07;
  **/
 public class Demo01 {
 
-    Node reversal(Node head){
-        return head;
+    //链表翻转 迭代法
+    static Node reversal(Node head) {
+        Node prev=null;
+        Node next;
+        Node curr =head;
+
+        while (curr!=null){
+            next=curr.next; //保存下一个
+            curr.next=prev; //将前一个的元素指针赋值给
+            prev = curr;
+            curr=next;  //移动循环
+
+
+
+        }
+        return prev;
     }
-    //链表创建
+
+    //链表创建 头插法
+    static Node CreateList() {
+        Node head = new Node(0);
+
+        for (int i  = 5; i >0; i--) {
+            Node node = new Node(i);
+            node.next=head.next;
+            head.next=node;
+        }
+
+        return head.next;
+    }
 
     public static void main(String[] args) {
-
+        Node node = Demo01.CreateList();
+        Node temp = node;
+        while (node != null){
+            System.out.println(node.value);
+            node=node.next;
+        }
+        System.out.println("==========");
+        node = Demo01.reversal(temp);
+        while (node != null){
+            System.out.println(node.value);
+            node=node.next;
+        }
     }
 }
--- Day01/src/main/java/com/zpy/test/test07/Node.java
+++ Day01/src/main/java/com/zpy/test/test07/Node.java
@@ -1,5 +1,8 @@
 package com.zpy.test.test07;
 
+/**
+ * 链表类
+ */
 public class Node {
     public Node(int value) {
         this.value = value;
